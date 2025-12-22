31a296d932942d30271f25fc4fa0d9f990677218
Anoulakdev <105956871+Anoulakdev@users.noreply.github.com>
2025-12-22 16:27:13 +0700

    22122025

--- src/modules/power/services/findone.ts
+++ src/modules/power/services/findone.ts
@@ -2,7 +2,18 @@ import { PrismaService } from '../../../prisma/prisma.service';
 import { NotFoundException } from '@nestjs/common';
 
 export async function findOnePower(prisma: PrismaService, id: number) {
-  const power = await prisma.power.findUnique({ where: { id } });
+  const power = await prisma.power.findUnique({
+    where: { id },
+    include: {
+      company: true,
+      voltage: true,
+      fuel: true,
+      contract: true,
+      branch: true,
+      region: true,
+      owner: true,
+    },
+  });
   if (!power) throw new NotFoundException('power not found');
   return power;
 }
